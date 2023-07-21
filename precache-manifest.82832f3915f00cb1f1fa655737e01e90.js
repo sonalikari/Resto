f45df47c671428377b5fc0d2c788aa021d55a06d
sonalikari <sonali211103@gmail.com>
2023-07-21 20:21:59 +0530

    Updates

---
+++ precache-manifest.82832f3915f00cb1f1fa655737e01e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7432a79b8b52f3a7649918e3fc0dbb28",
+    "url": "/Resto/index.html"
+  },
+  {
+    "revision": "0507e67d80366b85cf0f",
+    "url": "/Resto/static/css/main.54347523.chunk.css"
+  },
+  {
+    "revision": "d99b23c36f91b3bde24a",
+    "url": "/Resto/static/js/2.3490c37a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Resto/static/js/2.3490c37a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0507e67d80366b85cf0f",
+    "url": "/Resto/static/js/main.1ee6eaef.chunk.js"
+  },
+  {
+    "revision": "3dffb49bc98d8f0ca02c",
+    "url": "/Resto/static/js/runtime-main.b0400344.js"
+  }
+]);
